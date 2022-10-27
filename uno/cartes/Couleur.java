3f3f05193b13f19644049b3bb0b70da8e324ae52
JulieDespre <juliedespre5@etu.univ-lorraine.fr>
2022-10-27 15:52:02 +0200

    premier TP Uno 27/10/22

--- uno/cartes/Couleur.java
+++ uno/cartes/Couleur.java
@@ -1,18 +1,15 @@
 package uno.cartes;
 
 public enum Couleur {
-    JAUNE("jaune"),BLEU("bleu"),ROUGE("rouge"),VERT("vert");
-
+    JAUNE("jaune"),BLEU("bleu"),ROUGE("rouge"),VERT("vert"),NOIR("noir");
     private String couleur;
 
-    private Couleur (String couleur){
+    private Couleur (String couleur) {
         this.couleur=couleur;
     }
-
-    public String  getCouleur(){
+    public String getCouleur(){
         return couleur;
     }
-
     public String toString() {
         return couleur;
     }
