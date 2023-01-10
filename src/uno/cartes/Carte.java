7b4545a7f1ccf09786289c47ef2584441f7c5ee7
JulieDespre <juliedespre5@etu.univ-lorraine.fr>
2023-01-10 19:45:45 +0100

    08/01 uno fonctionnel

--- src/uno/cartes/Carte.java
+++ src/uno/cartes/Carte.java
@@ -26,76 +26,137 @@ public abstract class Carte {
      * @param uno
      * @param valeur qui correspond soit au chiffre de la carte soit à sa valeur
      * @param couleur qui est définie dans l'énumération couleur
-     * @exception AssertionError si mess est null
      */
     public Carte(Uno uno, int valeur, Couleur couleur) {
         this.valeur = valeur;
         this.couleur = couleur;
         this.uno = uno;
     }
+    /**
+     * Constructeur de carte sans uno
+     * @param valeur qui correspond soit au chiffre de la carte soit à sa valeur
+     * @param couleur qui est définie dans l'énumération couleur
+     */
 
     public Carte(int valeur, Couleur couleur) {
         this.valeur = valeur;
         this.couleur = couleur;
     }
-
+    /**
+     * Retourne valeur d'une carte
+     * @return la valeur d'une carte
+     */
     public int getValeur() {
         return valeur;
     }
 
+    /**
+     * Modifie la valeur d'une carte
+     * @param valeur qui est la nouvelle valeur a attribuer à la carte
+     */
     public void setValeur(int valeur) {
         this.valeur = valeur;
     }
 
+    /**
+     * permet d'accéder à la valeur de la carte choisie
+     * sans paramètre
+     */
     public Couleur getCouleur() {
 
         return couleur;
     }
 
+    /**
+     * Modifie la couleur d'une carte
+     * @param couleur qui est la nouvelle valeur a attribuer à la carte
+     */
     public void setCouleur(Couleur couleur) {
 
         this.couleur = couleur;
     }
+
+    /**
+     * permet à la carte d'avoir un effet lors d'une partie de uno, hiéritée aux classes filles
+     */
     public abstract void appliquerEffet();
 
+    /**
+     * retour true s'il s'agit d'une carte noire (joker ou plus4) sinon retourne false
+     */
     public boolean estSansCouleur(){
         if (this.getCouleur() == NOIR){
             return true;
         }
         return false;
     }
+
+    /**
+     * retourne true si l'instance de la carte en cours est de la de la même couleur qu'une carte choisie
+     * @param c qui est la carte avec laquelle on veut comparer notre carte
+     */
     public boolean estDeCouleurCompatibleAvec(Carte c) {
         if (this.getCouleur() == c.getCouleur()){
             return true;
         }
         return false;
     }
+
+    /**
+     * abstraite permet de savoir si une carte peut être recouverte par une autre
+     * @param c qui peut être recouvrir notre carte
+     */
     public abstract boolean peutEtreRecouvertPar(Carte c);
+
+    /**
+     * savoir si l'on peut poser carte chiffre sur notre carte
+     * @param c qui peut être posée sur notre carte
+     */
     public boolean peutEtrePoseeSur(Chiffre c){
 
         return estDeCouleurCompatibleAvec(c);
     }
 
+    /**
+     * savoir si l'on peut poser carte plus2 sur notre carte
+     * @param c qui peut être posée sur notre carte
+     */
     public boolean peutEtrePoseeSur(Plus2 c){
 
         return estDeCouleurCompatibleAvec(c);
     }
 
+    /**
+     * savoir si l'on peut poser carte plus4 sur notre carte
+     * @param c qui peut être posée sur notre carte
+     */
     public boolean peutEtrePoseeSur(Plus4 c){
 
         return estDeCouleurCompatibleAvec(c);
     }
 
+    /**
+     * savoir si l'on peut poser carte joker sur notre carte
+     * @param c qui peut être posée sur notre carte
+     */
     public boolean peutEtrePoseeSur(Joker c){
 
         return estDeCouleurCompatibleAvec(c);
     }
 
+    /**
+     * savoir si l'on peut poser carte changement de sens sur notre carte
+     * @param c qui peut être posée sur notre carte
+     */
     public boolean peutEtrePoseeSur(ChangementDeSens c){
 
         return estDeCouleurCompatibleAvec(c);
     }
 
+    /**
+     * savoir si l'on peut poser carte passe ton tour sur notre carte
+     * @param c qui peut être posée sur notre carte
+     */
     public boolean peutEtrePoseeSur(PasseTonTour c){
 
         return estDeCouleurCompatibleAvec(c);
--- src/uno/cartes/ChangementDeSens.java
+++ src/uno/cartes/ChangementDeSens.java
@@ -4,13 +4,23 @@ import uno.jeu.Uno;
 
 public class ChangementDeSens extends Carte{
 
-    public ChangementDeSens (Uno uno, int valeur, Couleur couleur){
+
+    public /**
+     * Constructeur de carte type spécifique à parti d'un uno, d'un entier et d'une couleur issue de l'énumération Couleur
+     * @param uno
+     * @param valeur qui correspond soit au chiffre de la carte soit à sa valeur
+     * @param couleur qui est définie dans l'énumération couleur
+     */ChangementDeSens (Uno uno, int valeur, Couleur couleur){
         super(uno, valeur, couleur);
         assert valeur>0 && valeur==20  : "la valeur de la carte changement de sens n'est pas bonne";
         assert couleur != Couleur.NOIR : "la couleur de la carte changemet de sens n'est pas bonne";
 
     }
-
+    /**
+     * Constructeur de carte type spécifique à parti d'un uno, d'un entier et d'une couleur issue de l'énumération Couleur
+     * @param valeur qui correspond soit au chiffre de la carte soit à sa valeur
+     * @param couleur qui est définie dans l'énumération couleur
+     */
     //constructeur sans uno
     public ChangementDeSens (int valeur, Couleur couleur){
         super(valeur, couleur);
@@ -23,15 +33,28 @@ public class ChangementDeSens extends Carte{
         uno.changer2sens();
     }
 
+    /**
+     * permet de savoir si une carte peut être recouvert
+     * @param c carte qui reconvre notre
+     */
     @Override
     public boolean peutEtreRecouvertPar(Carte c) {
         return c.peutEtrePoseeSur(this);
     }
 
+    /**
+     * permet de savoir si une carte peut être poser sur notre carte
+     * @param c carte spé  qui reconvre notre
+     */
     @Override
     public boolean peutEtrePoseeSur(ChangementDeSens c) {
         return true;
     }
+
+    /**
+     * compare le type et la couleur de la carte
+     * @param obj avec lequel on veut comparer notre carte
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof ChangementDeSens) {
@@ -43,6 +66,10 @@ public class ChangementDeSens extends Carte{
         }
     }
 
+    /**
+     * affiche le type et la couleur de notre carte
+     * sans paramètre
+     */
     public String toString(){
         return "ChangementDeSens, "+couleur;
     }
